c3b3b14093dd16239172fe88aabcd003bc70fe9e
forceedge01 <its.inevitable@hotmail.com>
2013-04-14 12:45:47 +0100

    Minor stuff

--- Application/Console/Lib/Console.php
+++ Application/Console/Lib/Console.php
@@ -2,6 +2,62 @@
 
 class Console {
 
+    public function init(){
+
+        if (!isset($_SERVER['SERVER_NAME'])) {
+
+            echo 'Welcome to Genesis console generator, please choose an option and proceed with the onscreen instructions.';
+            $this->linebreak(1);
+
+            while (true) {
+
+                $this->showAllOptions(getOptions());
+
+                echo 'Enter choice: ';
+
+                $line = $this->readUser();
+
+                $args = explode('--', $line);
+
+                $this->switchOption($args[0]);
+            }
+        } else {
+
+            echo '<h3>Welcome to Genesis CRUD generator, please choose an option and proceed with the onscreen instructions.</h3>';
+
+            $options = getOptions();
+
+            $bundle = new Bundle('html');
+
+            if (isset($_POST['submitOption'])) {
+
+                $option = $_POST['option'];
+
+                $this->switchOption($option[0]);
+            }
+
+            echo '<form method="post">';
+
+            foreach ($options as $option) {
+
+                echo '<input type="radio" name="option[]" value="' . $option . '">' . $option . '</input><br />';
+            }
+
+            echo '<h4>List of bundles installed</h4>';
+
+            $bundles = $bundle->readBundles(true);
+
+            foreach ($bundles as $bundle) {
+
+                echo '<input type="radio" name="bundleName[]" value="' . $bundle . '">' . $bundle . '</input><br />';
+            }
+
+            echo '<br />New Bundle Name: <input type="text" name="bundle"><br /><br />';
+
+            echo '<input type="submit" name="submitOption"></form>';
+        }
+    }
+
     public function unknownOption() {
 
         echo 'Unknown option!';
@@ -98,6 +154,50 @@ class Console {
                 echo chr(10) . chr(13);
             else
                 echo '<br />';
+        }
+    }
+
+    function switchOption($switch) {
+
+        $args = explode(':', $switch);
+
+        if ($args[0] == 'bundle') {
+
+            if (isset($_SERVER['SERVER_NAME'])) {
+
+                $bundle = new Bundle('html');
+
+                $bundle->name = ($_POST['bundle'] ? $_POST['bundle'] : $_POST['bundleName'][0] );
+
+            } else {
+
+                $bundle = new Bundle('console');
+            }
+        }
+
+        switch (strtolower($args[0])) {
+
+            case 'bundle':
+
+                switch ($args[1]) {
+
+                    case 'create':
+                        $bundle->createBundle();
+                        break;
+                    case 'delete':
+                        $bundle->deleteBundle();
+                        break;
+                    case '0':
+                    case 'exit':
+                        exit(0);
+                        break;
+                }
+                break;
+
+            default:
+                echo 'Exiting';
+                exit;
+                break;
         }
     }
 
--- Application/Console/index.php
+++ Application/Console/index.php
@@ -2,6 +2,15 @@
 
 require_once __DIR__ . '/../Configs/ConsoleDirs.php';
 
+function getOptions() {
+
+    return array(
+        'bundle:create',
+        'bundle:delete',
+        'exit'
+    );
+}
+
 function requireAll($directory) {
 
     $files = scandir($directory);
@@ -20,111 +29,8 @@ function requireAll($directory) {
     }
 }
 
-function getOptions() {
-
-    return array(
-        'bundle:create',
-        'bundle:delete',
-        'exit'
-    );
-}
-
 requireAll(CONSOLE_LIB_FOLDER);
 
-if (!isset($_SERVER['SERVER_NAME'])) {
-    $console = new Console();
+$console = new Console();
 
-    echo 'Welcome to Genesis console generator, please choose an option and proceed with the onscreen instructions.';
-    $console->linebreak(1);
-
-    while (true) {
-
-        $console->showAllOptions(getOptions());
-
-        echo 'Enter choice: ';
-
-        $line = $console->readUser();
-
-        $args = explode('--', $line);
-
-        switchOption($args[0]);
-    }
-} else {
-
-    echo '<h3>Welcome to Genesis CRUD generator, please choose an option and proceed with the onscreen instructions.</h3>';
-
-    $options = getOptions();
-
-    $bundle = new Bundle('html');
-
-    if (isset($_POST['submitOption'])) {
-
-        $option = $_POST['option'];
-
-        switchOption($option[0]);
-    }
-
-    echo '<form method="post">';
-
-    foreach ($options as $option) {
-
-        echo '<input type="radio" name="option[]" value="' . $option . '">' . $option . '</input><br />';
-    }
-
-    echo '<h4>List of bundles installed</h4>';
-
-    $bundles = $bundle->readBundles(true);
-
-    foreach ($bundles as $bundle) {
-
-        echo '<input type="radio" name="bundleName[]" value="' . $bundle . '">' . $bundle . '</input><br />';
-    }
-
-    echo '<br />New Bundle Name: <input type="text" name="bundle"><br /><br />';
-
-    echo '<input type="submit" name="submitOption"></form>';
-}
-
-function switchOption($switch) {
-
-    $args = explode(':', $switch);
-
-    if ($args[0] == 'bundle') {
-
-        if (isset($_SERVER['SERVER_NAME'])) {
-
-            $bundle = new Bundle('html');
-
-            $bundle->name = ($_POST['bundle'] ? $_POST['bundle'] : $_POST['bundleName'][0] );
-
-        } else {
-
-            $bundle = new Bundle('console');
-        }
-    }
-
-    switch (strtolower($args[0])) {
-
-        case 'bundle':
-
-            switch ($args[1]) {
-
-                case 'create':
-                    $bundle->createBundle();
-                    break;
-                case 'delete':
-                    $bundle->deleteBundle();
-                    break;
-                case '0':
-                case 'exit':
-                    exit(0);
-                    break;
-            }
-            break;
-
-        default:
-            echo 'Exiting';
-            exit;
-            break;
-    }
-}
+$console->init();

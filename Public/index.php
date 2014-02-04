71c6baa95582a707b24e44b8d31609c40b9e0d40
Wahab Qureshi <Wahab.Qureshi@digitalanimal.com>
2014-02-04 09:58:45 +0000

    Removed

--- Public/index.php
+++ Public/index.php
@@ -4,15 +4,7 @@
  *
  * Developer: Wahab Qureshi
  * Date: 12-02-2013
-<<<<<<< HEAD
-<<<<<<< HEAD
- * Version: 0.4.0-8
-=======
- * Version: 0.3.14-10
->>>>>>> 67cad59fa598240299abe34d7f218c03b99f9846
-=======
- * Version: 0.3.8-4
->>>>>>> 9dad8a6e0688f40e9a0ee5561e4ead45124bcd03
+ * Version: 0.4.1-9
  *
  */
 
